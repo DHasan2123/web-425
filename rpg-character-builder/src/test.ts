f2575e36ef761d92c9b63db40b7c402c80d6660e
DHasan2123 <dderany@ymail.com>
2025-04-11 22:01:38 -0500

    Initial commit

---
+++ rpg-character-builder/src/test.ts
@@ -0,0 +1,12 @@
+import 'zone.js/testing';
+import { getTestBed } from '@angular/core/testing';
+import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
+
+// First, initialize the Angular testing environment.
+getTestBed().initTestEnvironment(
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting()
+);
+
+// Then, discover and run all the tests.
+import './app.component.spec'; // This is where you would import your component's spec file
