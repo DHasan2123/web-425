a7a886460229d128d8d009fabf79046b950d12a7
DHasan2123 <dderany@ymail.com>
2025-04-17 21:17:19 -0500

    Initial commit

--- rpg-character-builder/src/app/app.routes.ts
+++ rpg-character-builder/src/app/app.routes.ts
@@ -1,23 +1,24 @@
 import { Routes } from '@angular/router';
-import { PlayersComponent } from './players/players.component';  // Make sure to update paths
+import { PlayersComponent } from './players/players.component';
 import { SigninComponent } from './signin/signin.component';
 import { CreateCharacterComponent } from './create-character/create-character.component';
 import { CreateGuildComponent } from './create-guild/create-guild.component';
 import { CharacterFactionComponent } from './character-faction/character-faction.component';
+import { AuthGuard } from './auth/auth.guard';  // Import the AuthGuard
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 export const routes: Routes = [
   { path: 'players', component: PlayersComponent },
-  { path: 'signin', component: SigninComponent },
-  { path: 'create-character', component: CreateCharacterComponent },
+  { path: 'signin', component: SigninComponent },  // Route for the Sign In component
+  { path: 'create-character', component: CreateCharacterComponent, canActivate: [AuthGuard] },  // Protect the CreateCharacter route with AuthGuard
   { path: 'create-guild', component: CreateGuildComponent },
   { path: 'character-faction', component: CharacterFactionComponent },
-  { path: '', redirectTo: '/players', pathMatch: 'full' }  // Default route
+  { path: '', redirectTo: '/players', pathMatch: 'full' }  // Default route to players
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],  // Configure RouterModule
-  exports: [RouterModule]  // Export RouterModule to use it in the app
+  imports: [RouterModule.forRoot(routes)],  // Configures routing in the app
+  exports: [RouterModule]  // Exports RouterModule so it's available across the app
 })
 export class AppRoutingModule {}
